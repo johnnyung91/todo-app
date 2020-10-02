1645aaa5a26e7e199bc15a254a204f2b762bd663
Johnny Ung <johnnyung91@gmail.com>
2020-10-02 15:04:38 -0700

    added basic mutation query to graphQL server

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,25 +1,11 @@
-const students = [{
-  id: 1,
-  firstName: "Johnny",
-  lastName: "Ung",
-  gradeLevel: "freshman",
-  email: "johnnyung91@gmail.com",
-  phoneNumber: "626-586-8079"
-}, {
-  id: 2,
-  firstName: "Michael",
-  lastName: "Zhu",
-  gradeLevel: "freshman",
-  email: "mzhu23@gmail.com",
-  phoneNumber: "626-123-4567"
-}]
+const listOfStudents = require("../index")
 
 function test(parent, args, context, info) {
   return "Hello World"
 }
 
 function getStudents(parent, args, context, info) {
-  return students
+  return listOfStudents.students
 }
 
 module.exports = {
