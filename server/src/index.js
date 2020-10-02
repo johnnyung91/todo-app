e72894d7c69dc4f6d9061f6d03664d10dd35ab7f
Johnny Ung <johnnyung91@gmail.com>
2020-10-02 13:24:00 -0700

    added Query resolver to get student information

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
 const Query = require("./resolvers/Query")
 
-
 const resolvers = {
   Query
 }
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,7 +1,28 @@
+const students = [{
+  id: 1,
+  firstName: "Johnny",
+  lastName: "Ung",
+  gradeLevel: "freshman",
+  email: "johnnyung91@gmail.com",
+  phoneNumber: "626-586-8079"
+}, {
+  id: 2,
+  firstName: "Michael",
+  lastName: "Zhu",
+  gradeLevel: "freshman",
+  email: "mzhu23@gmail.com",
+  phoneNumber: "626-123-4567"
+}]
+
 function test(parent, args, context, info) {
   return "Hello World"
 }
 
+function getStudents(parent, args, context, info) {
+  return students
+}
+
 module.exports = {
-  test
+  test,
+  getStudents
 }
